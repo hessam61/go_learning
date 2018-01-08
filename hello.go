a71a82e30c98974a82ab01a15933fbb24185ad5d
Hessam Shahriari <hessam61@gmail.com>
2018-01-07 19:41:57 -0500

    Basic usage of contants and iota

--- hello.go
+++ hello.go
@@ -7,9 +7,21 @@ type Salutation struct {
 	greeting string
 }
 
+const(
+	PI = 3.14
+	Language = "Go"
+)
+const (
+	A = iota
+	B = iota
+	C = iota
+)
+
 func main() {
 
 	var s = Salutation{name: "Joe", greeting: "Hello!"}
 
 	fmt.Println(s.name, s.greeting)
+	fmt.Println(PI, Language)
+	fmt.Println(A, B, C)
 }
