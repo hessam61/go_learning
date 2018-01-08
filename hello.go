8f53eca2e64349af3197d1d86e6b05ed1e8df77b
Hessam Shahriari <hessam61@gmail.com>
2018-01-07 19:22:29 -0500

    Basic use of a pointer

--- hello.go
+++ hello.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var message string = "Hello Go World!"
+
 	message := "Hello Go World!"
-	fmt.Println(message)
+
+	var greeting *string = &message
+
+	fmt.Println(message, *greeting)
 }
