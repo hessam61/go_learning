6de5ce390c6b172b095a30476ba24d9d45f91867
Hessam Shahriari <hessam61@gmail.com>
2018-01-14 17:39:52 -0500

    Add a basic if statement

---
+++ branching.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type Salutation struct {
+	name string
+	greeting string
+}
+
+type Printer func(string)()
+
+const(
+	PI = 3.14
+	Language = "Go"
+)
+
+func Greet(salutation Salutation, do Printer, isFormal bool){
+	message, alternate := CreateMessage(salutation.name, salutation.greeting)
+	if isFormal{
+		do(message)
+	}
+	do(alternate)
+}
+
+func CreateMessage(name string, greeting string) (message string, alternate string) {
+	fmt.Println(len(greeting))
+	message = greeting + " " + name
+	alternate = "HEY!"  + name
+	return
+}
+
+func Print(s string){
+	fmt.Print(s)
+}
+
+func Println(s string){
+	fmt.Println(s)
+}
+
+func CreatePrintFunction(custom string) Printer {
+	return func(s string) {
+		fmt.Println(s + custom)
+	}
+}
+
+func PrintCustom(s string, custom string){
+	fmt.Println(s + custom)
+}
+
+func main() {
+
+	var s = Salutation{name: "Joe", greeting: "Hello"}
+
+	Greet(s, CreatePrintFunction("!!"), true)
+
+}
